96490633c85f961aca2b7927d0b895740342cd55
icebreaker <theicebreaker007@gmail.com>
2013-08-12 21:11:07 +0300

    request version 2.0

--- lib/chromedriver/helper.rb
+++ lib/chromedriver/helper.rb
@@ -33,7 +33,7 @@ module Chromedriver
 
     def download_url
       downloads = GoogleCodeParser.new(open(DOWNLOAD_URL)).downloads
-      url = downloads.grep(/chromedriver_#{platform}_.*\.zip/).first
+      url = downloads.grep(/chromedriver_#{platform}_2\..*\.zip/).last
       url = "http:#{url}" if url !~ /^http/
       url
     end
